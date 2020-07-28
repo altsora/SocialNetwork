c5f64ae96b243d6e2dc6258dbf15239a8fd3fdb0
I-am-Anton-D <motohd@yandex.ru>
2020-07-28 22:57:02 +0300

    Merge with master branch

--- src/main/java/main/model/Person.java
+++ src/main/java/main/model/Person.java
@@ -1,12 +1,13 @@
 package main.model;
 
+import com.sun.istack.NotNull;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+
 
 /**
  * Entity for person
