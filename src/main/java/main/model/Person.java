0b3c5f0200b9d7aecdf23d7eeb4dc6c9e8339b7c
I-am-Anton-D <motohd@yandex.ru>
2020-07-28 15:00:38 +0300

    Init, empty project

---
+++ src/main/java/main/model/Person.java
@@ -0,0 +1,14 @@
+package main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Entity for person
+ */
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+}
