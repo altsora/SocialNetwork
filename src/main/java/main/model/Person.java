763837466283509b7419c12a6d6a0328f17a386a
Oleg Bech <oleg071984@gmail.com>
2020-07-29 09:24:56 +0300

    SN-2, SN-3: modify person entity and repo, add migration script for person table

--- src/main/java/main/model/Person.java
+++ src/main/java/main/model/Person.java
@@ -1,189 +1,59 @@
 package main.model;
 
-import com.sun.istack.NotNull;
-import java.util.Date;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 /**
- * Entity for person
+ * Class Person.
+ * Entity Person.
+ *
+ * @version 1.0
  */
-
+@Data
+@NoArgsConstructor
 @Entity
-@Table(name = "persons")
+@Table(name = "person")
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name")
     private String firstName;
-
-    @NotNull
+    @Column(name = "last_name")
     private String lastName;
-
-    @NotNull
-    private String eMail;
-
-    @NotNull
-    private Date regDate;
-
-    private Date birthDate;
-    private Date lastOnlineTime;
+    @CreationTimestamp
+    @Column(name = "reg_date", columnDefinition = "timestamp with time zone")
+    private LocalDateTime regDate;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    @Column(name = "e_mail")
+    private String email;
     private String phone;
     private String password;
     private String photo;
     private String about;
     private String city;
     private String country;
+    @Column(name = "confirmation_code")
     private String confirmationCode;
-    private String messagesPermission;
-    private boolean isApproved;
-    private boolean isBlocked;
-    private boolean isOnline;
-    private boolean isDeleted;
+    @Column(name = "is_approved")
+    private boolean approved;
+    private String messages_permission;
+    @UpdateTimestamp
+    @Column(name = "last_online_time", columnDefinition = "timestamp with time zone")
+    private LocalDateTime lastOnlineTime;
+    @Column(name = "is_blocked")
+    private boolean blocked;
+    @Column(name = "is_online")
+    private boolean online;
+    @Column(name = "is_deleted")
+    private boolean deleted;
 
-    public int getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(Date regDate) {
-        this.regDate = regDate;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Date getLastOnlineTime() {
-        return lastOnlineTime;
-    }
-
-    public void setLastOnlineTime(Date lastOnlineTime) {
-        this.lastOnlineTime = lastOnlineTime;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getConfirmationCode() {
-        return confirmationCode;
-    }
-
-    public void setConfirmationCode(String confirmationCode) {
-        this.confirmationCode = confirmationCode;
-    }
-
-    public String getMessagesPermission() {
-        return messagesPermission;
-    }
-
-    public void setMessagesPermission(String messagesPermission) {
-        this.messagesPermission = messagesPermission;
-    }
-
-    public boolean isApproved() {
-        return isApproved;
-    }
-
-    public void setApproved(boolean approved) {
-        isApproved = approved;
-    }
-
-    public boolean isBlocked() {
-        return isBlocked;
-    }
-
-    public void setBlocked(boolean blocked) {
-        isBlocked = blocked;
-    }
-
-    public boolean isOnline() {
-        return isOnline;
-    }
-
-    public void setOnline(boolean online) {
-        isOnline = online;
-    }
-
-    public boolean isDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-    }
 }
--- src/main/java/main/repositories/PersonRepository.java
+++ src/main/java/main/repositories/PersonRepository.java
@@ -1,14 +1,24 @@
 package main.repositories;
 
 import main.model.Person;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
- * Repository for persons
+ * Interface PersonRepository.
+ * Data layer for Person entity.
+ *
+ * @version 1.0
+ * @see org.springframework.data.jpa.repository.JpaRepository
  */
 
 @Repository
-public interface PersonRepository extends CrudRepository<Integer, Person> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    Optional<Person> findByFirstName(String firstName);
+    Optional<Person> findByLastName(String lastName);
+    Optional<Person> findByEmail(String email);
+    Optional<Person> findByPhone(String phone);
 }
