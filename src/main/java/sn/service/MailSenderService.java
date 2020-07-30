cc3cd8ba83531a2296c7045f169e2712bf53226b
Fyrklod <fyrklod@gmail.com>
2020-07-30 22:23:15 +0300

    #5 SN-5 из pom убран junit + сервис рассылки переделан на отправку html сообщений, а не текста как было до этого

--- src/main/java/sn/service/MailSenderService.java
+++ src/main/java/sn/service/MailSenderService.java
@@ -1,10 +1,14 @@
 package sn.service;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class MailSenderService {
@@ -14,12 +18,15 @@ public class MailSenderService {
     @Value("${spring.mail.username}")
     private String username;
 
-    public void send(String emailTo, String subject, String message){
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(username);
-        mailMessage.setTo(emailTo);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(message);
+    @SneakyThrows
+    public void send(String emailTo, String subject, String message) {
+        MimeMessage mailMessage = javaMailSender.createMimeMessage();
+        boolean multipart = false;
+        MimeMessageHelper helper = new MimeMessageHelper(mailMessage, multipart);
+        helper.setFrom(username);
+        helper.setTo(emailTo);
+        helper.setSubject(subject);
+        mailMessage.setContent(message, "text/html");
 
         javaMailSender.send(mailMessage);
     }
