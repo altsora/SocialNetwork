2d0ff808a4222125d49cb7a69427b1512d53c329
KEA <kjanni22@gmail.com>
2020-08-03 19:52:58 +0300

    SN-11 Update Response to ServiceResponse

---
+++ src/main/java/sn/api/response/ServiceResponse.java
@@ -0,0 +1,36 @@
+package sn.api.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ServiceResponse<T> {
+    /**
+     * Root entity for service response
+     *
+     * @param error Message, in case of error, this will be the reason
+     *              Сообщение, в случае ошибки здесь будет причина
+     * @param timestamp Creation time - example: 1559751301818
+     * @param total Elements number in the list
+     *              Количество элементов в списке
+     * @param offset Indent from the beginning of the list
+     *               Отступ от начала списка
+     * @param perPage Elements per page
+     *                Количество элементов на страницу
+     */
+    private String error;
+    private Number timestamp;
+    private Integer total;
+    private Integer offset;
+    private Integer perPage;
+    private T data;
+
+
+    public ServiceResponse() {
+        this.timestamp = Timestamp.valueOf(LocalDateTime.now()).getTime();
+    }
+}
