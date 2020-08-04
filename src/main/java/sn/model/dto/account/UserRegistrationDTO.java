cd6af501f1bf7477e3f78919eb4077167c45e5ed
I-am-Anton-D <motohd@yandex.ru>
2020-08-04 18:00:18 +0300

    Implementation registry and recovery password

---
+++ src/main/java/sn/model/dto/account/UserRegistrationDTO.java
@@ -0,0 +1,56 @@
+package sn.model.dto.account;
+
+import java.lang.reflect.Field;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * DTO for registration
+ */
+
+@Data
+@AllArgsConstructor
+public class UserRegistrationDTO {
+    private String email;
+    private String passwd1;
+    private String passwd2;
+    private String firstName;
+    private String lastName;
+    private String code;
+
+    public boolean selfCheck() throws IllegalAccessException {
+        Field[] fields = this.getClass().getFields();
+        for (Field f:fields) {
+            if (((String) f.get(this)).isEmpty() || f.get(this)==null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getPasswd1() {
+        return passwd1;
+    }
+
+    public String getPasswd2() {
+        return passwd2;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+
+
--- src/main/java/sn/service/IAccountService.java
+++ src/main/java/sn/service/IAccountService.java
@@ -1,7 +1,12 @@
 package sn.service;
 
+import sn.model.dto.account.UserRegistrationDTO;
+
 public interface IAccountService {
 
     boolean setNewPassword(String recoveryCode, String password);
     boolean changeEmail(String newEmail);
+    boolean register(UserRegistrationDTO userRegistrationDTO);
+    boolean recoveryPassword(String email);
+
 }
