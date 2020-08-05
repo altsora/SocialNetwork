fce1bcea2007f2dbdd21cbe101afde2af36c013f
Oleg Bech <oleg071984@gmail.com>
2020-08-05 19:05:10 +0300

    SN-8 прикручены ServiceResponse и EmailSenderService

--- src/main/java/sn/api/ResponseDataMessage.java
+++ src/main/java/sn/api/ResponseDataMessage.java
@@ -2,9 +2,10 @@ package sn.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import sn.api.response.AbstractResponse;
 
 @Data
 @AllArgsConstructor
-public class ResponseDataMessage {
+public class ResponseDataMessage extends AbstractResponse {
     private String message;
 }
--- src/main/java/sn/controller/AccountController.java
+++ src/main/java/sn/controller/AccountController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sn.api.ResponseDataMessage;
-import sn.api.ServiceResponse;
+import sn.api.response.ServiceResponse;
 import sn.model.dto.account.EmailDTO;
 import sn.model.dto.account.UserRegistrationDTO;
 import sn.model.dto.account.recoveryPassword.RecoveryFormData;
@@ -17,7 +17,7 @@ import sn.service.IPersonService;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/account")
+@RequestMapping("/account")
 public class AccountController {
 
     @Autowired
@@ -31,66 +31,65 @@ public class AccountController {
     //Регистрация пользователя
     @PostMapping("/register")
     public ResponseEntity<ServiceResponse<ResponseDataMessage>> register(
-        @RequestBody UserRegistrationDTO userRegistrationDTO)
-        throws IllegalAccessException {
+            @RequestBody UserRegistrationDTO userRegistrationDTO)
+            throws IllegalAccessException {
         if (!userRegistrationDTO.selfCheck()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Regitstration data empty or null")));
+                    new ServiceResponse<>("Bad request", new ResponseDataMessage("Regitstration data empty or null")));
         }
         return accountService.register(userRegistrationDTO) ? ResponseEntity.status(HttpStatus.OK).body(
-            new ServiceResponse<>(new ResponseDataMessage("Registration successfull"))
+                new ServiceResponse<>(new ResponseDataMessage("Registration successfull"))
         ) :
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
-            );
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
+                );
     }
 
     @PutMapping("/password/recovery")
     public ResponseEntity<ServiceResponse<ResponseDataMessage>> recoveryPassword(@RequestBody EmailDTO emailDTO) {
         if (Strings.isNullOrEmpty(emailDTO.getEmail())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Email is null or empty")));
+                    new ServiceResponse<>("Bad request", new ResponseDataMessage("Email is null or empty")));
         }
         return accountService.recoveryPassword(emailDTO.getEmail()) ?
-            ResponseEntity.status(HttpStatus.OK).body(
-                new ServiceResponse<>(new ResponseDataMessage("Recovery information was sent to e-mail"))
-            ) :
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
-            );
-        return null;
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new ServiceResponse<>(new ResponseDataMessage("Recovery information was sent to e-mail"))
+                ) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
+                );
     }
 
     @PutMapping("/password/set")
     public ResponseEntity<ServiceResponse<ResponseDataMessage>> setPassword(
-        @RequestBody RecoveryFormData recoveryFormData) {
+            @RequestBody RecoveryFormData recoveryFormData) {
         if (Strings.isNullOrEmpty(recoveryFormData.getToken()) || Strings
-            .isNullOrEmpty(recoveryFormData.getPassword())) {
+                .isNullOrEmpty(recoveryFormData.getPassword())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Recovery data null or empty")));
+                    new ServiceResponse<>("Bad request", new ResponseDataMessage("Recovery data null or empty")));
         }
         return accountService.setNewPassword(recoveryFormData.getToken(), recoveryFormData.getPassword()) ?
-            ResponseEntity.status(HttpStatus.OK).body(
-                new ServiceResponse<>(new ResponseDataMessage("Person password successfully recovered"))
-            ) :
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
-            );
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new ServiceResponse<>(new ResponseDataMessage("Person password successfully recovered"))
+                ) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
+                );
     }
 
     @PutMapping("/email")
     public ResponseEntity<ServiceResponse<ResponseDataMessage>> setEmail(@RequestBody EmailDTO emailDTO) {
         if (Strings.isNullOrEmpty(emailDTO.getEmail())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Email is null or empty")));
+                    new ServiceResponse<>("Bad request", new ResponseDataMessage("Email is null or empty")));
         }
         return accountService.changeEmail(emailDTO.getEmail()) ?
-            ResponseEntity.status(HttpStatus.OK).body(
-                new ServiceResponse<>(new ResponseDataMessage("Person email successfully changed"))
-            ) :
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
-            );
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new ServiceResponse<>(new ResponseDataMessage("Person email successfully changed"))
+                ) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable"))
+                );
     }
 
     //Редактирование настроек оповещения
