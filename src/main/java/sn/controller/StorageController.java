5817c9c94cee452f79c32b93d6add2cda1f67fdf
egalimsarov <e.galimsarov@gmail.com>
2020-08-07 14:09:58 +0500

    StorageResponse теперь наследуется от AbstractResponse

--- src/main/java/sn/controller/StorageController.java
+++ src/main/java/sn/controller/StorageController.java
@@ -2,32 +2,30 @@ package sn.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import sn.response.AbstractResponse;
+import sn.api.response.AbstractResponse;
+import sn.api.response.StorageResponse;
 import sn.service.StorageService;
 
 import java.io.IOException;
-import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/storage")
+@RequestMapping("/storage")
 public class StorageController {
 
     @Autowired
     private StorageService storageService;
 
-    @PostMapping(value = "storage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping//(value = "storage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity uploadFile(@RequestParam String type,
                                      @RequestParam(value = "file", required = false)
                                      MultipartFile file) throws IOException {
-        AbstractResponse response = new AbstractResponse(storageService, type, file);
-        Map<String, Object> uploadResult = response.uploadFileResponse();
-        return new ResponseEntity(uploadResult, response.isSuccess() ? HttpStatus.OK :
-                HttpStatus.BAD_REQUEST);
+        AbstractResponse response = storageService.uploadFileResponse(type, file);
+        // Если нужно получить сслыку на файл в хранилище, то используем это:
+        // ((StorageResponse) response).getResponse().get("url")
+        return new ResponseEntity(response, ((StorageResponse) response).isSuccess()
+                ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
     }
-
-//        data.setRawFileURL(map.get("url").toString());
 }
--- src/main/java/sn/service/StorageService.java
+++ src/main/java/sn/service/StorageService.java
@@ -4,6 +4,7 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import sn.api.response.StorageResponse;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,13 +14,16 @@ import java.util.Objects;
 @Service
 public class StorageService {
     // Cloudinary cloud_name, API_Key and API_Secret
-    private static final String CLOUDINARY_CLOUD_NAME = "javapro-socialnetwork-studygroup-7";
-    private static final String CLOUDINARY_API_KEY = "751383328333813";
-    private static final String CLOUDINARY_API_SECRET = "11mJVXn8IE-H5oftzDbYbycc3Ig";
+    private static final String
+            CLOUDINARY_CLOUD_NAME = "javapro-socialnetwork-studygroup-7";
+    private static final String
+            CLOUDINARY_API_KEY = "751383328333813";
+    private static final String
+            CLOUDINARY_API_SECRET = "11mJVXn8IE-H5oftzDbYbycc3Ig";
 
-    public Map<String, Object> uploadFileResponse(String type, MultipartFile file)
+    public StorageResponse uploadFileResponse(String type, MultipartFile file)
             throws IOException {
-        Map<String, Object> cloudinaryUrl;
+        StorageResponse cloudinaryUrl = new StorageResponse();
         Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", CLOUDINARY_CLOUD_NAME,
                 "api_key", CLOUDINARY_API_KEY,
@@ -31,18 +35,21 @@ public class StorageService {
             @SuppressWarnings("unchecked")
             Map<String, Object> result =
                     cloudinary.uploader().upload(convFile, ObjectUtils.emptyMap());
-            cloudinaryUrl = result;
+            cloudinaryUrl.setSuccess(true);
+            cloudinaryUrl.setResponse(result);
         } catch (IOException e) {
-            System.out.println("Could not upload file to Cloundinary from MultipartFile " +
-                    file.getOriginalFilename()+ e.toString());
+            System.out.println("Could not upload file to Cloundinary from " +
+                    "MultipartFile " + file.getOriginalFilename()+ e.toString());
             throw e;
         }
         return cloudinaryUrl;
     }
 
-    private static File multipartToFile(MultipartFile image) throws IllegalStateException,
+    private static File multipartToFile(MultipartFile image)
+            throws IllegalStateException,
             IOException {
-        File convFile = new File(Objects.requireNonNull(image.getOriginalFilename()));
+        File convFile = new File(Objects.
+                requireNonNull(image.getOriginalFilename()));
         image.transferTo(convFile);
         return convFile;
     }
