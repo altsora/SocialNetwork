ca912139f4b16d62331ad25e12ac34811ba3bf9c
altsora <vergun_a.s@mail.ru>
2020-08-10 15:42:45 +0300

    Промежуточный этап 2

--- src/main/java/sn/controller/ProfileController.java
+++ src/main/java/sn/controller/ProfileController.java
@@ -27,6 +27,14 @@ public class ProfileController {
 
     //==================================================================================================================
 
+    /**
+     * Метод getCurrentUser.
+     * Получение текущего пользователя.
+     * GET запрос /api/v1/users/me
+     *
+     * @return 200 - пользователь успешно получен;
+     * 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @GetMapping("/me")
     public ResponseEntity<ServiceResponse<AbstractResponse>> getCurrentUser() {
         //TODO: проверка (в каком случае может быть отрицательный ответ?)
@@ -66,6 +74,15 @@ public class ProfileController {
         return ResponseEntity.ok(new ServiceResponse<>(personResponse));
     }
 
+    /**
+     * Метод editCurrentUser.
+     * Редактирование текущего пользователя.
+     * PUT запрос /api/v1/users/me
+     *
+     * @param personEditBody - тело запроса в формате JSON. Содержит данные новые данные пользователя.
+     * @return 200 - пользователь успешно отредактирован;
+     * 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @PutMapping("/me")
     public ResponseEntity<ServiceResponse<AbstractResponse>> editCurrentUser(@RequestBody PersonEditBody personEditBody) {
         //TODO: проверка (в каком случае может быть отрицательный ответ?)
@@ -80,7 +97,6 @@ public class ProfileController {
         // в базу
         Person person = personService.updatePerson(personEditBody);
 
-        //ЭТО БУДЕТ В СЕРВИСЕ
         person.setFirstName(personEditBody.getFirstName());
         person.setLastName(personEditBody.getLastName());
         //TODO: конвертировать время в long
@@ -94,7 +110,6 @@ public class ProfileController {
 //        person.setCity();
 //        person.setCountry();
         person.setMessagesPermission(personEditBody.getMessagesPermission());
-        //ЭТО БУДЕТ В СЕРВИСЕ
 
         //TODO: нет данных, откуда эти объекты берутся.
         // По приходящим ID должны устанавливаться новые значения
@@ -122,15 +137,34 @@ public class ProfileController {
         return ResponseEntity.ok(new ServiceResponse<>(personResponse));
     }
 
+    /**
+     * Метод getUserById.
+     * Удаление текущего пользователя.
+     * DELETE запрос /api/v1/users/{id}
+     *
+     * @return 200 - пользователь удалён;
+     * 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @DeleteMapping("/me")
     public ResponseEntity<ServiceResponse<AbstractResponse>> deleteCurrentUser() {
         //TODO: Получить ID текущего пользователя и по нему удалять в базе
         boolean personDeleted = personService.deleteById(getCurrentUserId());
         return personDeleted ?
-                ResponseEntity.ok(new ServiceResponse<>(new ResponseDataMessage("ok"))) :
-                ResponseEntity.badRequest().body(new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable")));
+                ResponseEntity
+                        .ok(new ServiceResponse<>(new ResponseDataMessage("ok"))) :
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body(new ServiceResponse<>("Unauthorized", new ResponseDataMessage("User is not authorized")));
     }
 
+    /**
+     * Метод getUserById.
+     * Получить пользователя по id.
+     * GET запрос /api/v1/users/{id}
+     *
+     * @param personId - ID пользователя, которого надо получить.
+     * @return 200 - получение пользователя по указанному идентификатору;
+     * 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<ServiceResponse<AbstractResponse>> getUserById(@PathVariable(value = "id") long personId) {
         Person person = personService.findById(personId);
@@ -164,6 +198,17 @@ public class ProfileController {
         return ResponseEntity.ok(new ServiceResponse<>(personResponse));
     }
 
+    /**
+     * Метод getWallEntriesByUserId.
+     * Получение записей на стене пользователя.
+     * GET запрос /api/v1/users/{id}/wall
+     *
+     * @param personId    - ID пользователя, со стены которого требуется получить записи.
+     * @param offset      - Отступ от начала результирующего списка публикаций.
+     * @param itemPerPage - Количество публикаций из результирующего списка, которые представлены для отображения.
+     * @return 200 - получение результирующего списка с публикациями на стене пользователя;
+     * 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @GetMapping("/{id}/wall")
     public ResponseEntity<ServiceResponse<AbstractResponse>> getWallEntriesByUserId(
             @PathVariable(value = "id") long personId,
@@ -219,13 +264,24 @@ public class ProfileController {
         }
         int total = postService.getTotalCountPostsByPersonId(personId);
         //TODO: Список с постами не наследуется от AbstractResponse
-        return ResponseEntity.ok(new ServiceResponse<>(total,offset, itemPerPage, wallPosts));
+        return ResponseEntity.ok(new ServiceResponse<>(total, offset, itemPerPage, wallPosts));
     }
 
+    /**
+     * Метод setWallEntriesByUserId.
+     * Добавление публикации на стену пользователя.
+     * POST запрос /api/v1/users/{id}/wall
+     *
+     * @param personId        - ID пользователя, который публикует записи.
+     * @param publishDate     - Дата публикации, установленная пользователем.
+     * @param postRequestBody - тело запроса в формате JSON. Содержит данные о новой публикации.
+     * @return 200 - запись готова к публикации к назначенному времени;
+     * 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @PostMapping("/{id}/wall")
-    public ResponseEntity<Object> setWallEntriesByUserId(
+    public ResponseEntity<ServiceResponse<AbstractResponse>> setWallEntriesByUserId(
             @PathVariable(value = "id") long personId,
-            @PathVariable(value = "publish_date", required = false) Long publishDate,
+            @RequestParam(value = "publish_date", required = false) Long publishDate,
             @RequestBody PostRequestBody postRequestBody
     ) {
         //TODO: если пользователь не авторизован?
@@ -277,21 +333,85 @@ public class ProfileController {
         return ResponseEntity.ok(new ServiceResponse<>(newPost));
     }
 
+    /**
+     * Метод findUser.
+     * Поиск пользователей по указанным параметрам.
+     * GET запрос /api/v1/users/search
+     *
+     * @param firstName   - Имя пользователей.
+     * @param lastName    - Фамилия пользователей.
+     * @param ageFrom     - Минимальный возраст пользователей.
+     * @param ageTo       - Максимальный возраст пользователей.
+     * @param countryId   - Идентификатор страны пользователей.
+     * @param cityId      - Идентификатор города пользователей.
+     * @param offset      - Отступ от начала результирующего списка пользователей.
+     * @param itemPerPage - Количество пользователей из результирующего списка, которые представлены для отображения.
+     * @return 200 - Возврат списка пользователей, подходящих по указанным параметрам;
+     * 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @GetMapping("/search")
-    public ResponseEntity<Object> findUser() {
+    public ResponseEntity<Object> findUser(
+            @RequestParam(value = "first_name", required = false) String firstName,
+            @RequestParam(value = "last_name", required = false) String lastName,
+            @RequestParam(value = "age_from", required = false) Integer ageFrom,
+            @RequestParam(value = "age_to", required = false) Integer ageTo,
+            @RequestParam(value = "country_id", required = false) Integer countryId,
+            @RequestParam(value = "city_id", required = false) Integer cityId,
+            @RequestParam(value = "offset", defaultValue = "0") Integer offset,
+            @RequestParam(value = "itemPerPage", defaultValue = "20") Integer itemPerPage
+    ) {
+
         //todo
         return null;
     }
 
+    /**
+     * Метод blockUserById.
+     * Блокировка пользователя.
+     * PUT запрос /api/v1/users/block/{id}
+     *
+     * @param personId - ID пользователя, которого надо заблокировать.
+     * @return 200 - пользователь заблокирован; 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @PutMapping("/block/{id}")
-    public ResponseEntity<Object> blockUserById() {
-        //todo
-        return null;
+    public ResponseEntity<ServiceResponse<AbstractResponse>> blockUserById(@PathVariable(value = "id") long personId) {
+        Person person = personService.findById(personId);
+        person.setBlocked(true);
+        //TODO: ошибка авторизации
+        if (false) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ServiceResponse<>("Unauthorized", new ResponseDataMessage("User is not authorized")));
+        }
+        //TODO: Если регистрация успешна - boolean method
+        boolean block = personService.changeBlockStatus(personId);
+        return block ?
+                ResponseEntity.ok(new ServiceResponse<>(new ResponseDataMessage("ok"))) :
+                ResponseEntity.badRequest().body(new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable")));
+        ;
     }
 
+    /**
+     * Метод unblockUserById.
+     * Разблокировка пользователя.
+     * DELETE запрос /api/v1/users/block/{id}
+     *
+     * @param personId - ID пользователя, которого надо разблокировать.
+     * @return 200 - пользователь разблокирован; 400 - произошла ошибка; 401 - ошибка авторизации.
+     */
     @DeleteMapping("/block/{id}")
-    public ResponseEntity<Object> unblockUserById() {
-        //todo
-        return null;
+    public ResponseEntity<ServiceResponse<AbstractResponse>> unblockUserById(@PathVariable(value = "id") long personId) {
+        Person person = personService.findById(personId);
+        person.setBlocked(false);
+        //TODO: ошибка авторизации
+        if (false) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ServiceResponse<>("Unauthorized", new ResponseDataMessage("User is not authorized")));
+        }
+        //TODO: Если регистрация успешна - boolean method
+        boolean unblock = personService.changeBlockStatus(personId);
+        return unblock ?
+                ResponseEntity.ok(new ServiceResponse<>(new ResponseDataMessage("ok"))) :
+                ResponseEntity.badRequest().body(new ServiceResponse<>("Bad request", new ResponseDataMessage("Service unavailable")));
+        ;
     }
 }
