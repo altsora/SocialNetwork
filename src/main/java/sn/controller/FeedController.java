450fa6ea873785fd339124c29a35ef3427d9e87f
FormAToz <kycok2@yandex.ru>
2020-08-19 21:50:37 +0300

    Реализация логики FeedController. Создание FeedService.

--- src/main/java/sn/controller/FeedController.java
+++ src/main/java/sn/controller/FeedController.java
@@ -4,14 +4,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sn.api.response.ServiceResponse;
 
+/**
+ * REST-контроллер для работы с лентой новостей.
+ */
 @RestController
 @RequestMapping("/feeds")
 public class FeedController {
+    private final FeedController feedController;
 
+    public FeedController(FeedController feedController) {
+        this.feedController = feedController;
+    }
+
+    /**
+     * Получение списка новостей
+     * @param name Текст новости для поиска
+     * @param offset Отступ от начала списка
+     * @param itemPerPage Количество элементов на страницу
+     * @return 200 - список новостей получен успешно, 400 - ошибка во время получения списка, 401 - ошибка авторизации.
+     */
     @GetMapping
-    public ResponseEntity<Object> getFeeds() {
-        //todo
-        return null;
+    public ResponseEntity<ServiceResponse> getFeeds(String name, int offset, int itemPerPage) {
+        return feedController.getFeeds(name, offset, itemPerPage);
     }
 }
