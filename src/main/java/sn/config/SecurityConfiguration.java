1d988a7eafa0e331ce103308d4ca1ce970a62f78
Apollo <apo11o@me.com>
2020-08-24 13:16:21 +0300

    JSON request and response added

--- src/main/java/sn/config/SecurityConfiguration.java
+++ src/main/java/sn/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package sn.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,7 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests().anyRequest().authenticated().and()
-            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+            .addFilter(new JwtAuthenticationFilter(authenticationManager(), getApplicationContext()))
             .addFilter(new JwtAuthorizationFilter(authenticationManager())).sessionManagement()
             .sessionCreationPolicy(
                 SessionCreationPolicy.STATELESS);
