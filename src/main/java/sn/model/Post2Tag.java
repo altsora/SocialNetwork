a991c8ba0853de0a36bcdf64c76f922c7c913701
@derevninaa <a.a.derevnin@gmail.com>
2020-08-24 13:59:28 +0300

    Add Tag and Post2Tag Entities

--- src/main/java/sn/model/Post2Tag.java
+++ src/main/java/sn/model/Post2Tag.java
@@ -1,22 +1,20 @@
 package sn.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "tag")
 @Data
-public class Tag {
+public class Post2Tag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "tag")
-    private String tag;
+
+    private Post post;
+    private Tag tag;
 }
