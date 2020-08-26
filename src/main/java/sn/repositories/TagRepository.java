afaedefdc6589e18100312d941d32f627608ce3f
@derevninaa <a.a.derevnin@gmail.com>
2020-08-26 15:33:22 +0300

    Add Tag and Post2Tag Entities

--- src/main/java/sn/repositories/TagRepository.java
+++ src/main/java/sn/repositories/TagRepository.java
@@ -1,8 +1,5 @@
 package sn.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import sn.model.Tag;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
+public class TagRepository {
 
 }
--- src/main/java/sn/service/impl/TagService.java
+++ src/main/java/sn/service/impl/TagService.java
@@ -1,40 +1,5 @@
 package sn.service.impl;
 
+public class TagService {
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import sn.api.response.MessageResponse;
-import sn.api.response.tagresponse.TagResponseDTO;
-import sn.model.Tag;
-import sn.repositories.TagRepository;
-import sn.service.ITagService;
-
-@Service
-public class TagService implements ITagService {
-
-    private final TagRepository tagRepository;
-
-    @Autowired
-    public TagService(TagRepository tagRepository) {
-        this.tagRepository = tagRepository;
-    }
-
-    @Override
-    public TagResponseDTO createTag(String tag) {
-        Tag newTag = new Tag(tag);
-        tagRepository.save(newTag);
-        return new TagResponseDTO(newTag.getId(), newTag.getTag());
-    }
-
-    @Override
-    public MessageResponse deleteTagById(long id) {
-        MessageResponse response = new MessageResponse();
-        if (tagRepository.findById(id).isPresent()) {
-            tagRepository.deleteById(id);
-            response.setMessage("ok");
-        } else {
-            response.setMessage("No Such Tag");
-        }
-        return response;
-    }
 }
