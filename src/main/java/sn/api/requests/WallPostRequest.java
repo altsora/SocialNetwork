9d65f1da939f3335ae8ff65e507d725f42a2fdd9
KEA <kjanni22@gmail.com>
2020-09-14 19:04:49 +0300

    added all test for existing ProfileController methods

--- src/main/java/sn/api/requests/WallPostRequest.java
+++ src/main/java/sn/api/requests/WallPostRequest.java
@@ -3,11 +3,13 @@ package sn.api.requests;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class WallPostRequest {
     @JsonProperty("title")
     private String title;
