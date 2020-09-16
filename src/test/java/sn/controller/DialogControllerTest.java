21baba10150b4543d7bd8d6174cea7049df98d3a
altsora <vergun_a.s@mail.ru>
2020-09-16 23:50:01 +0300

    Возвращена аннотация JsonValue. Добавлены новые тесты

--- src/test/java/sn/controller/DialogControllerTest.java
+++ src/test/java/sn/controller/DialogControllerTest.java
@@ -6,19 +6,18 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import sn.api.response.*;
-import sn.model.Dialog;
+import sn.api.response.AbstractResponse;
+import sn.api.response.MessageIdResponse;
+import sn.api.response.ServiceResponse;
 import sn.service.impl.DialogService;
 import sn.service.impl.MessageService;
 import sn.utils.ErrorUtil;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,6 +37,9 @@ public class DialogControllerTest extends AbstractWebController {
     private MessageService messageService;
     @MockBean
     private DialogService dialogService;
+
+    private static ResponseEntity<ServiceResponse<AbstractResponse>> badRequestResponse = ErrorUtil.badRequest("bad");
+    private static ResponseEntity<ServiceResponse> okResponse = ResponseEntity.ok(new ServiceResponse<>());
 
     private static String asJsonString(final Object obj) {
         try {
@@ -60,35 +62,82 @@ public class DialogControllerTest extends AbstractWebController {
         this.mockMvc = MockMvcBuilders.standaloneSetup(dialogController).build();
     }
 
-    @Test
-    public void getUserDialogList() {
+    //==================================================================================================================
 
+    /**
+     * Получение списка диалогов авторизованного пользователя. Bad request.
+     */
+    @Test
+    public void getUserDialogListIsBadRequest() throws Exception {
+        int offset = 0;
+        int itemPerPage = 20;
+        String query = "test query";
+
+        Mockito.doReturn(badRequestResponse)
+                .when(dialogService)
+                .findPersonDialogsWithQuery(query, offset, itemPerPage);
+
+        mockMvc.perform(get("/dialogs")
+                .param("query", query)
+                .param("offset", String.valueOf(offset))
+                .param("itemPerPage", String.valueOf(itemPerPage)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
     }
 
+    /**
+     * Получение списка диалогов авторизованного пользователя. OK.
+     */
     @Test
-    public void createDialogIsOk() throws Exception{
-        // Момент первый: возникает ошибка из-за одноимённых полей (DialogResponse#dialogDataSet и DialogResponse#dialogMessages).
-        // Пока что закомментил @JsonValue над dialogMessages
+    public void getUserDialogListIsOk() throws Exception {
+        int offset = 0;
+        int itemPerPage = 20;
+        String query = "test query";
 
-        // Момент второй: не проходит DialogResponse через asJsonString (скрины кидал). А должен
+        Mockito.doReturn(okResponse)
+                .when(dialogService)
+                .findPersonDialogsWithQuery(query, offset, itemPerPage);
+
+        mockMvc.perform(get("/dialogs")
+                .param("query", query)
+                .param("offset", String.valueOf(offset))
+                .param("itemPerPage", String.valueOf(itemPerPage)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    /**
+     * Создание диалога. Bad request.
+     */
+    @Test
+    public void createDialogIsBadRequest() throws Exception {
         DialogController.UserIdsRequest request = new DialogController.UserIdsRequest();
-        DialogResponse dialogResponse1 = DialogResponse.builder().build();
-        System.out.println("DialogResponse (asJsonString): " + asJsonString(dialogResponse1));  // Получается null
-        System.out.println("DialogResponse (constructor) : " + dialogResponse1);
-        System.out.println();
-        ErrorResponse errorResponse = ErrorResponse.builder().build();
-        System.out.println("ErrorResponse (asJsonString): " + asJsonString(errorResponse));
-        System.out.println("ErrorResponse (constructor): " + errorResponse);
 
-        //==============================================================================================================
+        Mockito.doReturn(badRequestResponse)
+                .when(dialogService)
+                .createDialog(request);
 
-        Dialog dialog = new Dialog();
-        dialog.setId(5);
-        ServiceResponse<DialogResponse> serviceResponse =
-                new ServiceResponse<>(DialogResponse.builder().dialogId(dialog.getId()).build());
-        ResponseEntity<ServiceResponse<DialogResponse>> responseEntity = ResponseEntity.status(HttpStatus.OK).body(serviceResponse);
+        mockMvc.perform(post("/dialogs")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
 
-        Mockito.doReturn(responseEntity)
+    /**
+     * Создание диалога. OK.
+     */
+    @Test
+    public void createDialogIsOk() throws Exception {
+        DialogController.UserIdsRequest request = new DialogController.UserIdsRequest();
+
+        Mockito.doReturn(okResponse)
                 .when(dialogService)
                 .createDialog(request);
 
@@ -98,20 +147,97 @@ public class DialogControllerTest extends AbstractWebController {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().string(asJsonString(responseEntity.getBody())))
                 .andReturn();
     }
 
+    /**
+     * Успешное получение общего кол-ва непрочитанных сообщений. OK.
+     */
     @Test
-    public void getUnreadedMessagesCount() {
+    public void getUnreadedMessagesCountIsOk() throws Exception {
+        Mockito.doReturn(okResponse)
+                .when(dialogService)
+                .getUnreadMessagesCount();
+
+        mockMvc.perform(get("/dialogs/unreaded"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
     }
 
+    /**
+     * Получение общего кол-ва непрочитанных сообщений. Bad request.
+     */
     @Test
-    public void deleteDialogById() {
+    public void getUnreadedMessagesCountIsBadRequest() throws Exception {
+        Mockito.doReturn(badRequestResponse)
+                .when(dialogService)
+                .getUnreadMessagesCount();
+
+        mockMvc.perform(get("/dialogs/unreaded"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
     }
 
+    /**
+     * Удаление свзяи текущего пользователя и диалога с указанным id. OK.
+     */
     @Test
-    public void addUsers() {
+    public void deleteDialogByIdIsOk() throws Exception {
+        long dialogId = 1;
+
+        Mockito.doReturn(okResponse)
+                .when(dialogService)
+                .deleteDialog(dialogId);
+
+        mockMvc.perform(delete("/dialogs/{id}", dialogId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    /**
+     * Удаление свзяи текущего пользователя и диалога с указанным id. Bad request.
+     */
+    @Test
+    public void deleteDialogByIdIsBadRequest() throws Exception {
+        long dialogId = 1;
+
+        Mockito.doReturn(badRequestResponse)
+                .when(dialogService)
+                .deleteDialog(dialogId);
+
+        mockMvc.perform(delete("/dialogs/{id}", dialogId))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    /**
+     * Добавить пользователей в диалог. OK.
+     */
+    @Test
+    public void addUsersIsOk() throws Exception{
+        long dialogId = 1;
+
+
+        mockMvc.perform(delete("/dialogs/{id}/users", dialogId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    /**
+     * Удаление свзяи текущего пользователя и диалога с указанным id. Bad request.
+     */
+    @Test
+    public void addUsersIsBadRequest() {
     }
 
     @Test
@@ -156,9 +282,7 @@ public class DialogControllerTest extends AbstractWebController {
         long dialogId = 1;
         long messageId = 2;
 
-        ServiceResponse<MessageIdResponse> serviceResponse = new ServiceResponse<>(MessageIdResponse.builder().build());
-
-        Mockito.doReturn(ResponseEntity.ok(serviceResponse))
+        Mockito.doReturn(ResponseEntity.ok(new ServiceResponse<>()))
                 .when(dialogService)
                 .removeMessage(dialogId, messageId);
 
@@ -166,7 +290,6 @@ public class DialogControllerTest extends AbstractWebController {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().string(asJsonString(serviceResponse)))
                 .andReturn();
     }
 
@@ -180,9 +303,7 @@ public class DialogControllerTest extends AbstractWebController {
         long dialogId = 1;
         long messageId = 2;
 
-        ResponseEntity<ServiceResponse<AbstractResponse>> responseEntity = ErrorUtil.badRequest("bad");
-
-        Mockito.doReturn(responseEntity)
+        Mockito.doReturn(ErrorUtil.badRequest("bad"))
                 .when(dialogService)
                 .removeMessage(dialogId, messageId);
 
@@ -190,7 +311,6 @@ public class DialogControllerTest extends AbstractWebController {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().string(asJsonString(responseEntity.getBody())))
                 .andReturn();
     }
 
